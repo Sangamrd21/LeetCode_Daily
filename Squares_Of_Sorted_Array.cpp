c0ed2e572440e03546b4d80741daaa3b5a7706a9
Sangamrd21 <sangurd21@gmail.com>
2024-04-18 13:11:07 +0530

    Squares_Of_Sorted_Array code updated

--- Squares_Of_Sorted_Array.cpp
+++ Squares_Of_Sorted_Array.cpp
@@ -4,13 +4,40 @@ public:
         for(int i=0;i<size(nums);i++){
             nums[i]=nums[i]*nums[i];
         }
-        for(int i=0;i<size(nums);i++){
-            for(int j=i+1;j<size(nums);j++){
-                if(nums[j]<nums[i]){
-                    temp=nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                }
+        // for(int i=0;i<size(nums);i++){
+        //     for(int j=i+1;j<size(nums);j++){
+        //         if(nums[j]<nums[i]){
+        //             temp=nums[j];
+        //             nums[j]=nums[i];
+        //             nums[i]=temp;
+        //         }
+        //     }
+        // }
+        int sizeOfArray=size(nums);
+        selSort(nums, sizeOfArray);
+        return nums;
+    }
+    void swap(vector<int>& arr, int m, int n){
+        int temp = arr[m];
+        arr[m] = arr[n];
+        arr[n] = temp; 
+    }
+    int locOfSmallest(vector<int>& arr, int s, int e){
+        int i=s;
+        int j=i;
+        while(i<=e){
+            if(arr[i]<arr[j]){
+                j=i;
             }
+            i++;
+        }
+        return j;
+    }
+    void selSort(vector<int>& arr, int n){
+        int i=0;
+        while(i<n-1){
+            int j = locOfSmallest(arr, i, n-1);
+            swap(arr, i, j);
+            i++;
         }
     }
