7f70dd9460e75c669cbedea03bcc7b1355fc22cb
Sangamrd21 <sangurd21@gmail.com>
2024-04-23 00:27:20 +0530

    Remove_Duplicates_From_Sorted_Array problem code updated

--- Remove_Duplicates_From_Sorted_Array.cpp
+++ Remove_Duplicates_From_Sorted_Array.cpp
@@ -1,19 +1,18 @@
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n=size(nums);
+        int n = size(nums);
         int k=n;
-        int l=0;
         for(int i=1;i<k;i++){
-            if(nums[i-1]==nums[i]){
-                removeElement(nums, i, k);
-                k--;
-                i--;
+            int j=i;
+            while(j<k && nums[i-1]==nums[j]){
+                j++;
+            }
+            if(j>i){
+                k=k-(j-i);
+                for(int m=i;m<k;m++){
+                    nums[m]=nums[m+j-i];
+                }
             }
         }
         return k;
-    }
-    void removeElement(vector<int>& nums, int i, int n){
-        for(int j=i+1;j<n;j++){
-            nums[j-1]=nums[j];
-        }
     }
