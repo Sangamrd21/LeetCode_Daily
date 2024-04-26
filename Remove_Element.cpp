6cb2755ffc1adc6a1e65658f5ce241e37a223154
Sangamrd21 <sangurd21@gmail.com>
2024-04-26 08:57:37 +0530

    Remove Element problem code upadated with in-place method

--- Remove_Element.cpp
+++ Remove_Element.cpp
@@ -1,19 +1,11 @@
 public:
     int removeElement(vector<int>& nums, int val) {
         int k=0;
-        int i=0;
-        int l=0;
-        int n=size(nums);
-        while(i<n-l){
-            if(nums[i]==val){
-                for(int j=i+1;j<n;j++){
-                    nums[j-1]=nums[j];
-                }
-                l++;
+        for(int i=0;i<size(nums);i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
             }
-            else{
-                k++; i++;
-            }
-        }
+        }    
         return k;
     }
